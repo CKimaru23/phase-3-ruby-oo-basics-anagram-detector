dba17092914e5a6d2c5b258a302761fa299a55a5
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2023-02-09 08:50:51 +0300

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word.downcase
+  end
+
+  def match(possible_anagrams)
+    possible_anagrams.select do |possible_anagram|
+      sorted_word = @word.chars.sort.join
+      sorted_possible_anagram = possible_anagram.downcase.chars.sort.join
+      sorted_word == sorted_possible_anagram && @word != possible_anagram.downcase
+    end
+  end
+end
